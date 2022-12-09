a75f530503de394385de66bea3d07bf00a845260
acemindua <acemind.ua@gmail.com>
2022-12-09 22:33:01 +0200

    --update ui

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix
+    .js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css')
     .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+    .sourceMaps()
+    .version();
